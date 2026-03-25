c8e73d8e05710a201f06c1aab439060a8768873c
ExtaSsS4106 <ExtaZzZ123456789@yandex.ru>
2026-03-25 18:43:50 +0300

    feat: added basic api_fetcher

--- src/scripts/data/fetcher.js
+++ src/scripts/data/fetcher.js
@@ -1,3 +1,57 @@
 class fetcher{
+    constructor(){
+        this.url = "https://script.google.com/macros/s/AKfycbxtM6plQ32ks16IUEZCZNLaVIn5CedilGWd4RjMGA27wVCRF2kj1MB7m4exrUSqjvr_/exec";
+    }
 
+
+    async getAllPatterns(){
+        try{
+            const response = await fetch(`${this.url}?action=getAllPatterns`);
+            const data = await response.json();
+            console.log(JSON.stringify(data));
+            return data;
+        }catch{
+            return {"data": "error"}
+        }
+    }
+
+    
+    async getPatternById(id){
+        if (!id){
+            return "no id"
+        }
+        try{
+            const response = await fetch(`${this.url}?action=getPatternById&id=${id}`);
+            const data = await response.json();
+            console.log(JSON.stringify(data));
+            return data;
+        }catch{
+            return {"data": "error"}
+        }
+    }   
+
+    async getCategories(){
+        try{
+            const response = await fetch(`${this.url}?action=getCategories`);
+            const data = await response.json();
+            console.log(JSON.stringify(data));
+            return data;
+        }catch{
+            return {"data": "error"}
+        }
+    }  
+
+    async getPatternsByCategories(id){
+        if (!id){
+            return "no id"
+        }
+        try{
+            const response = await fetch(`${this.url}?action=getPatternsByCategories&category_id=${id}`);
+            const data = await response.json();
+            console.log(JSON.stringify(data));
+            return data;
+        }catch{
+            return {"data": "error"}
+        }
+    }
 }
